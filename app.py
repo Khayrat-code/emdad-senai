a28b4607749e83091168f088d654397960451610
Khayrat-code <72605091+Khayrat-code@users.noreply.github.com>
2025-04-21 00:00:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -268,5 +268,5 @@ def supplier_profile(supplier_id):
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
