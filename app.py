8f61fb7228d5931ae0099922745b5e93b966e5db
Khayrat-code <72605091+Khayrat-code@users.noreply.github.com>
2025-04-21 00:36:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import os
+from werkzeug.unils import secure_filenma
 
 app = Flask(__name__)
 app.secret_key = 'secure_key_here'
